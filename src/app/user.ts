2be2ee3b1e80c066439517b44cf9dbe07bcd6447
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-05-23 19:16:41 +0300

    Define the constructor arguments

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,15 @@
 export class User {
-}
+    constructor(
+      public avatar_url: any,
+      public login: string,
+      public bio: string,
+      public public_repos: number,
+      public public_gists: number,
+      public followers: number,
+      public following: number,
+      public url: string,
+      public location:string,
+      public email: string
+  ) {}
+  }
+  
