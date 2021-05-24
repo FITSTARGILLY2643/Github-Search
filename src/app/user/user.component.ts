95ab745edaf43f07046b26742ee878224a86b0f9
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-05-24 23:10:58 +0300

    Insert the default userch name to be searched

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GitService } from '../git.service';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user'
+import { Repository } from '../repository'
 
 @Component({
   selector: 'app-user',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  user?:User;
+  userRep:any;
+  
+    constructor(private gitService:GitService) {
+      this.userRep = new Repository('', '', '', '');
+     }
+     performSearch(searchTerm:any) {
+      this.gitService.userRequest(searchTerm).then((success)=>{
+        this.user = this.gitService.user
+      })
+      
+      this.gitService.displayRepos(searchTerm).then((success) => {
+        this.userRep = this.gitService.userRep
+      
+      },
+        (error) => {
+          console.log(error)
+        }
+      ) 
+    }
+    ngOnInit(): void {
+      this.performSearch('FITSTARGILLY2643')
+    }
+  
+    
   }
-
-}
